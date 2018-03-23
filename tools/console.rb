e0f3a1f0d3a5d422aef4f3d27f1fdd8973d4f7f7
azeelleeza <leezaluncheon@gmail.com>
2018-03-23 11:00:09 -0400

    complete

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,54 @@ def reload
   load 'config/environment.rb'
 end
 
+###Objects
+
+anne = Customer.new("Annabelle","Appleton")
+billy = Customer.new("William","Brown")
+cathy = Customer.new("Catherine","Connors")
+richie = Customer.new("Richard","Rathers")
+dick = Customer.new("Richard","Robertson")
+
+vietnamese = Restaurant.new("Pho 14")
+caribbean = Restaurant.new("Negril")
+pizza = Restaurant.new("Paisano's")
+texmex = Restaurant.new("Chipotle")
+
+first = anne.add_review(vietnamese,"good")
+second = billy.add_review(caribbean,"bad")
+third = billy.add_review(pizza,"ugly")
+fourth = cathy.add_review(texmex,"good")
+fifth = richie.add_review(vietnamese,"bad")
+sixth = dick.add_review(texmex,"ugly")
+
+###PUTS
+
+puts first.customer
+puts second.customer
+puts third.customer
+puts "\n"
+puts fourth.restaurant
+puts fifth.restaurant
+puts sixth.restaurant
+puts "\n"
+puts Customer.all.inspect
+puts Customer.all_names.inspect
+puts Restaurant.all.inspect
+puts Review.all.inspect
+puts "\n"
+puts Customer.find_by_name("Annabelle Appleton").inspect
+puts Customer.find_by_name("Richard Robertson").inspect
+puts Customer.find_all_by_first_name("Richard").inspect
+puts "\n"
+puts cathy.full_name
+puts "\n"
+puts Restaurant.find_by_name("Pho 14").inspect
+puts "\n"
+puts vietnamese.reviews.inspect
+puts vietnamese.customers.inspect
+
+
+
+
+
 Pry.start
