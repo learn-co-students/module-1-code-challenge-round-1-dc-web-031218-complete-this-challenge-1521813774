2b6d1430a7e10b665c04b3823223b739cafb4d9c
Austin Wolfe <wolfe_austin@yahoo.com>
2018-03-23 10:58:03 -0400

    Completed before refactoring

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,35 @@
 class Customer
   attr_accessor :first_name, :last_name
-
+  @@all = []
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    names = name.split(" ")
+    all.find{|n| n.first_name == names[0] && n.last_name == names[1]}
+  end
+
+  def self.find_all_by_first_name(name)
+    all.select{|n| n.first_name == name}
+  end
+
+  def self.all_names
+    self.all.map{|customer| customer.full_name}
+  end
+
+  def add_review(restaurant, content)
+    review = Review.new(restaurant, self, content)
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,28 @@ def reload
   load 'config/environment.rb'
 end
 
-Pry.start
+
+
+#Pry.start
+
+c1 = Customer.new("Hector", "Garcia")
+c2 = Customer.new("Andrew", "Temple")
+c3 = Customer.new("Tasha", "Williams")
+c4 = Customer.new("Austin", "Wolfe")
+c5 = Customer.new("Austin", "Wolfe")
+c4.full_name
+c2.full_name
+
+apple = Restaurant.new("Applebees")
+shoneys = Restaurant.new("Shoneys")
+outback = Restaurant.new("Outback")
+hooters = Restaurant.new("Hooters")
+
+r1 = c1.add_review(apple, "Good Shit")
+c2.add_review(outback, "Could've been better")
+binding.pry
+
+apple = Restaurant.new("Applebees")
+shoneys = Restaurant.new("Shoneys")
+outback = Restaurant.new("Outback")
+hooters = Restaurant.new("Hooters")
